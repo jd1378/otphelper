32f4ff193863440c300cf10f737d7460e5c9090c
Javad Mnjd <javad.mnjd@hotmail.com>
2023-09-17 12:20:57 +0330

    fix: update theme color for older android versions

--- app/src/main/java/io/github/jd1378/otphelper/ui/theme/Theme.kt
+++ app/src/main/java/io/github/jd1378/otphelper/ui/theme/Theme.kt
@@ -14,24 +14,130 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
+import com.example.compose.md_theme_dark_background
+import com.example.compose.md_theme_dark_error
+import com.example.compose.md_theme_dark_errorContainer
+import com.example.compose.md_theme_dark_inverseOnSurface
+import com.example.compose.md_theme_dark_inversePrimary
+import com.example.compose.md_theme_dark_inverseSurface
+import com.example.compose.md_theme_dark_onBackground
+import com.example.compose.md_theme_dark_onError
+import com.example.compose.md_theme_dark_onErrorContainer
+import com.example.compose.md_theme_dark_onPrimary
+import com.example.compose.md_theme_dark_onPrimaryContainer
+import com.example.compose.md_theme_dark_onSecondary
+import com.example.compose.md_theme_dark_onSecondaryContainer
+import com.example.compose.md_theme_dark_onSurface
+import com.example.compose.md_theme_dark_onSurfaceVariant
+import com.example.compose.md_theme_dark_onTertiary
+import com.example.compose.md_theme_dark_onTertiaryContainer
+import com.example.compose.md_theme_dark_outline
+import com.example.compose.md_theme_dark_outlineVariant
+import com.example.compose.md_theme_dark_primary
+import com.example.compose.md_theme_dark_primaryContainer
+import com.example.compose.md_theme_dark_scrim
+import com.example.compose.md_theme_dark_secondary
+import com.example.compose.md_theme_dark_secondaryContainer
+import com.example.compose.md_theme_dark_surface
+import com.example.compose.md_theme_dark_surfaceTint
+import com.example.compose.md_theme_dark_surfaceVariant
+import com.example.compose.md_theme_dark_tertiary
+import com.example.compose.md_theme_dark_tertiaryContainer
+import com.example.compose.md_theme_light_background
+import com.example.compose.md_theme_light_error
+import com.example.compose.md_theme_light_errorContainer
+import com.example.compose.md_theme_light_inverseOnSurface
+import com.example.compose.md_theme_light_inversePrimary
+import com.example.compose.md_theme_light_inverseSurface
+import com.example.compose.md_theme_light_onBackground
+import com.example.compose.md_theme_light_onError
+import com.example.compose.md_theme_light_onErrorContainer
+import com.example.compose.md_theme_light_onPrimary
+import com.example.compose.md_theme_light_onPrimaryContainer
+import com.example.compose.md_theme_light_onSecondary
+import com.example.compose.md_theme_light_onSecondaryContainer
+import com.example.compose.md_theme_light_onSurface
+import com.example.compose.md_theme_light_onSurfaceVariant
+import com.example.compose.md_theme_light_onTertiary
+import com.example.compose.md_theme_light_onTertiaryContainer
+import com.example.compose.md_theme_light_outline
+import com.example.compose.md_theme_light_outlineVariant
+import com.example.compose.md_theme_light_primary
+import com.example.compose.md_theme_light_primaryContainer
+import com.example.compose.md_theme_light_scrim
+import com.example.compose.md_theme_light_secondary
+import com.example.compose.md_theme_light_secondaryContainer
+import com.example.compose.md_theme_light_surface
+import com.example.compose.md_theme_light_surfaceTint
+import com.example.compose.md_theme_light_surfaceVariant
+import com.example.compose.md_theme_light_tertiary
+import com.example.compose.md_theme_light_tertiaryContainer
 
-private val DarkColorScheme =
-    darkColorScheme(primary = Purple80, secondary = PurpleGrey80, tertiary = Pink80)
-
-private val LightColorScheme =
+private val lightColors =
     lightColorScheme(
-        primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40
+        primary = md_theme_light_primary,
+        onPrimary = md_theme_light_onPrimary,
+        primaryContainer = md_theme_light_primaryContainer,
+        onPrimaryContainer = md_theme_light_onPrimaryContainer,
+        secondary = md_theme_light_secondary,
+        onSecondary = md_theme_light_onSecondary,
+        secondaryContainer = md_theme_light_secondaryContainer,
+        onSecondaryContainer = md_theme_light_onSecondaryContainer,
+        tertiary = md_theme_light_tertiary,
+        onTertiary = md_theme_light_onTertiary,
+        tertiaryContainer = md_theme_light_tertiaryContainer,
+        onTertiaryContainer = md_theme_light_onTertiaryContainer,
+        error = md_theme_light_error,
+        onError = md_theme_light_onError,
+        errorContainer = md_theme_light_errorContainer,
+        onErrorContainer = md_theme_light_onErrorContainer,
+        outline = md_theme_light_outline,
+        background = md_theme_light_background,
+        onBackground = md_theme_light_onBackground,
+        surface = md_theme_light_surface,
+        onSurface = md_theme_light_onSurface,
+        surfaceVariant = md_theme_light_surfaceVariant,
+        onSurfaceVariant = md_theme_light_onSurfaceVariant,
+        inverseSurface = md_theme_light_inverseSurface,
+        inverseOnSurface = md_theme_light_inverseOnSurface,
+        inversePrimary = md_theme_light_inversePrimary,
+        surfaceTint = md_theme_light_surfaceTint,
+        outlineVariant = md_theme_light_outlineVariant,
+        scrim = md_theme_light_scrim,
+    )
 
-        /* Other default colors to override
-        background = Color(0xFFFFFBFE),
-        surface = Color(0xFFFFFBFE),
-        onPrimary = Color.White,
-        onSecondary = Color.White,
-        onTertiary = Color.White,
-        onBackground = Color(0xFF1C1B1F),
-        onSurface = Color(0xFF1C1B1F),
-        */
-        )
+private val darkColors =
+    darkColorScheme(
+        primary = md_theme_dark_primary,
+        onPrimary = md_theme_dark_onPrimary,
+        primaryContainer = md_theme_dark_primaryContainer,
+        onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+        secondary = md_theme_dark_secondary,
+        onSecondary = md_theme_dark_onSecondary,
+        secondaryContainer = md_theme_dark_secondaryContainer,
+        onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+        tertiary = md_theme_dark_tertiary,
+        onTertiary = md_theme_dark_onTertiary,
+        tertiaryContainer = md_theme_dark_tertiaryContainer,
+        onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+        error = md_theme_dark_error,
+        onError = md_theme_dark_onError,
+        errorContainer = md_theme_dark_errorContainer,
+        onErrorContainer = md_theme_dark_onErrorContainer,
+        outline = md_theme_dark_outline,
+        background = md_theme_dark_background,
+        onBackground = md_theme_dark_onBackground,
+        surface = md_theme_dark_surface,
+        onSurface = md_theme_dark_onSurface,
+        surfaceVariant = md_theme_dark_surfaceVariant,
+        onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+        inverseSurface = md_theme_dark_inverseSurface,
+        inverseOnSurface = md_theme_dark_inverseOnSurface,
+        inversePrimary = md_theme_dark_inversePrimary,
+        surfaceTint = md_theme_dark_surfaceTint,
+        outlineVariant = md_theme_dark_outlineVariant,
+        scrim = md_theme_dark_scrim,
+    )
 
 @Composable
 fun OtpHelperTheme(
@@ -46,14 +152,17 @@ fun OtpHelperTheme(
           val context = LocalContext.current
           if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkColors
+        else -> lightColors
       }
   val view = LocalView.current
   if (!view.isInEditMode) {
     SideEffect {
       (view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
-      (view.context as Activity).window.decorView.setBackgroundColor(colorScheme.background.toArgb())
+      (view.context as Activity)
+          .window
+          .decorView
+          .setBackgroundColor(colorScheme.background.toArgb())
 
       ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = !darkTheme
     }
