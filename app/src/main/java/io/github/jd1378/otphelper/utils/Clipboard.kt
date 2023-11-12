4a87039a01e8f1ca96d79c0a2e138c41d8cc0a37
Javad Mnjd <javad.mnjd@hotmail.com>
2023-11-13 00:00:00 +0330

    chore: mark clipboard content as sensitive

--- app/src/main/java/io/github/jd1378/otphelper/utils/Clipboard.kt
+++ app/src/main/java/io/github/jd1378/otphelper/utils/Clipboard.kt
@@ -2,10 +2,13 @@ package io.github.jd1378.otphelper.utils
 
 import android.app.Activity
 import android.content.ClipData
+import android.content.ClipDescription
 import android.content.ClipboardManager
 import android.content.Context
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import android.os.PersistableBundle
 import android.widget.Toast
 import io.github.jd1378.otphelper.R
 
@@ -18,7 +21,17 @@ class Clipboard {
 
       var toastText: Int =
           if (clipboardManager !== null) {
-            clipboardManager.setPrimaryClip(ClipData.newPlainText(code, code))
+            clipboardManager.setPrimaryClip(
+                ClipData.newPlainText(code, code).apply {
+                  description.extras =
+                      PersistableBundle().apply {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                          putBoolean(ClipDescription.EXTRA_IS_SENSITIVE, true)
+                        } else {
+                          putBoolean("android.content.extra.IS_SENSITIVE", true)
+                        }
+                      }
+                })
             R.string.code_copied_to_clipboard
           } else {
             R.string.code_failed_to_access_clipboard
