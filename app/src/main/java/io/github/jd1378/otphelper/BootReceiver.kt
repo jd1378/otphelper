1852163d5d18befc581b420af4bf6f3d1fe3ae5a
Javad Mnjd <javad.mnjd@hotmail.com>
2024-05-12 18:18:52 +0330

    fix: the permission check in bootreceiver

--- app/src/main/java/io/github/jd1378/otphelper/BootReceiver.kt
+++ app/src/main/java/io/github/jd1378/otphelper/BootReceiver.kt
@@ -3,16 +3,15 @@ package io.github.jd1378.otphelper
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Build
+import androidx.core.app.NotificationManagerCompat
 import io.github.jd1378.otphelper.utils.NotificationHelper
 
 class BootReceiver : BroadcastReceiver() {
   override fun onReceive(context: Context, intent: Intent?) {
     if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-      if (context.checkSelfPermission(
-          android.Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE,
-      ) == PackageManager.PERMISSION_GRANTED) {
+      if (NotificationManagerCompat.getEnabledListenerPackages(context)
+          .contains(context.packageName)) {
         context.startService(Intent(context, NotificationListener::class.java))
       } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
         // the bug is only present in android 11 and before it seems
