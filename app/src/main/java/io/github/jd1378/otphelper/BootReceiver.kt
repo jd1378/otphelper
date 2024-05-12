47960f9c6bb8b3ba3da8fa32422ee6948140bc95
Javad Mnjd <javad.mnjd@hotmail.com>
2024-05-12 18:21:02 +0330

    Revert "fix: only show permission revoked permission for <= 11"
    This reverts commit 4fff01322ba260cefd6504e302d2df0d9b2b30a8.

--- app/src/main/java/io/github/jd1378/otphelper/BootReceiver.kt
+++ app/src/main/java/io/github/jd1378/otphelper/BootReceiver.kt
@@ -3,7 +3,6 @@ package io.github.jd1378.otphelper
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import androidx.core.app.NotificationManagerCompat
 import io.github.jd1378.otphelper.utils.NotificationHelper
 
@@ -13,8 +12,7 @@ class BootReceiver : BroadcastReceiver() {
       if (NotificationManagerCompat.getEnabledListenerPackages(context)
           .contains(context.packageName)) {
         context.startService(Intent(context, NotificationListener::class.java))
-      } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
-        // the bug is only present in android 11 and before it seems
+      } else {
         NotificationHelper.sendPermissionRevokedNotif(context)
       }
     }
