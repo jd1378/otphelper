4fff01322ba260cefd6504e302d2df0d9b2b30a8
Javad Mnjd <javad.mnjd@hotmail.com>
2024-05-12 16:18:54 +0330

    fix: only show permission revoked permission for <= 11

--- app/src/main/java/io/github/jd1378/otphelper/BootReceiver.kt
+++ app/src/main/java/io/github/jd1378/otphelper/BootReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import io.github.jd1378.otphelper.utils.NotificationHelper
 
 class BootReceiver : BroadcastReceiver() {
@@ -13,7 +14,8 @@ class BootReceiver : BroadcastReceiver() {
           android.Manifest.permission.BIND_NOTIFICATION_LISTENER_SERVICE,
       ) == PackageManager.PERMISSION_GRANTED) {
         context.startService(Intent(context, NotificationListener::class.java))
-      } else {
+      } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+        // the bug is only present in android 11 and before it seems
         NotificationHelper.sendPermissionRevokedNotif(context)
       }
     }
