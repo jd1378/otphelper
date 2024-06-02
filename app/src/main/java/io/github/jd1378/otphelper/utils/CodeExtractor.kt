0e1373d0fb4d4da4c607f551c1ce984d77682aa0
antonioalopezfernandez <5657720+antonioalopezfernandez@users.noreply.github.com>
2024-06-02 17:40:15 +0200

    fix: PIN keyword added in order to work with Spanish app

--- app/src/main/java/io/github/jd1378/otphelper/utils/CodeExtractor.kt
+++ app/src/main/java/io/github/jd1378/otphelper/utils/CodeExtractor.kt
@@ -14,7 +14,7 @@ class CodeExtractor {
             "contraseña",
             "c[oó]digo",
             "clave",
-            "PIN",
+            "\\bPIN\\W",
             "验证码",
             "校验码",
             "識別碼",
