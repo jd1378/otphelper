52aa5de2be20b924779406b626bc8a5497792ffa
Javad Mnjd <javad.mnjd@hotmail.com>
2024-07-09 04:22:37 +0330

    chore: upgrade agp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("com.android.application") version "8.3.2" apply false
-  id("com.android.library") version "8.3.2" apply false
+  id("com.android.application") version "8.4.1" apply false
+  id("com.android.library") version "8.4.1" apply false
   id("org.jetbrains.kotlin.android") version "1.9.24" apply false
   id("com.google.dagger.hilt.android") version "2.50" apply false
   id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
