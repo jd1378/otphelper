560e9a25a6242a64552504d55b5b1e1478d9d1b4
Javad Mnjd <javad.mnjd@hotmail.com>
2024-07-20 01:14:19 +0330

    fix: send permission revoked message only when necessary

--- app/src/main/java/io/github/jd1378/otphelper/BootReceiver.kt
+++ app/src/main/java/io/github/jd1378/otphelper/BootReceiver.kt
@@ -3,7 +3,6 @@ package io.github.jd1378.otphelper
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.util.Log
 import io.github.jd1378.otphelper.NotificationListener.Companion.isNotificationListenerServiceEnabled
 import io.github.jd1378.otphelper.NotificationListener.Companion.tryReEnableNotificationListener
@@ -22,12 +21,8 @@ class BootReceiver : BroadcastReceiver() {
           )
         }
         tryReEnableNotificationListener(context)
-      }
-      when (Build.VERSION.SDK_INT) {
-        Build.VERSION_CODES.Q,
-        Build.VERSION_CODES.R -> {
-          NotificationHelper.sendPermissionRevokedNotif(context)
-        }
+      } else {
+        NotificationHelper.sendPermissionRevokedNotif(context)
       }
     }
   }
