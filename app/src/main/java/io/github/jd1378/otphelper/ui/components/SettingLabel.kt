cd90230ddf0953996f461c0d9030614e811fe844
Javad Mnjd <javad.mnjd@hotmail.com>
2024-07-20 16:53:33 +0330

    fix: give text of setting label weight of 1f

--- app/src/main/java/io/github/jd1378/otphelper/ui/components/SettingLabel.kt
+++ app/src/main/java/io/github/jd1378/otphelper/ui/components/SettingLabel.kt
@@ -1,12 +1,22 @@
 package io.github.jd1378.otphelper.ui.components
 
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun RowScope.SettingLabel(
+    text: String,
+) {
+  SettingLabel(text = text, modifier = Modifier.weight(1f))
+}
 
 @Composable
 fun SettingLabel(
     text: String,
+    modifier: Modifier = Modifier,
 ) {
-  Text(text = text, fontWeight = FontWeight.Medium)
+  Text(text = text, fontWeight = FontWeight.Medium, modifier = modifier)
 }
