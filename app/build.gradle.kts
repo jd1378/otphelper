8b2bc2494d27074da7c55fb7adb613840ce3265c
Javad Mnjd <javad.mnjd@hotmail.com>
2025-03-28 03:37:47 +0330

    fix: migration path issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "io.github.jd1378.otphelper"
     minSdk = 24
     targetSdk = 35
-    versionCode = 37
-    versionName = "1.18.1"
+    versionCode = 38
+    versionName = "1.18.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables { useSupportLibrary = true }
