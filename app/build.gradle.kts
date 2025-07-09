da5b085b9d47e642e2ca04d232c1185673ac8be5
Javad Mnjd <javad.mnjd@hotmail.com>
2025-07-09 20:06:24 +0330

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,20 +63,20 @@ android {
   applicationVariants.all(ApplicationVariantAction())
 }
 
-val protobufVersion = "3.25.6"
+val protobufVersion = "3.25.8"
 
 dependencies {
-  implementation(platform("androidx.compose:compose-bom:2025.05.00"))
-  androidTestImplementation(platform("androidx.compose:compose-bom:2025.05.00"))
+  implementation(platform("androidx.compose:compose-bom:2025.06.01"))
+  androidTestImplementation(platform("androidx.compose:compose-bom:2025.06.01"))
 
-  implementation("androidx.core:core-ktx:1.15.0")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-  implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+  implementation("androidx.core:core-ktx:1.16.0")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+  implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.1")
   implementation("androidx.activity:activity-compose:1.10.1")
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
-  implementation("androidx.datastore:datastore-preferences:1.1.3")
+  implementation("androidx.datastore:datastore-preferences:1.1.7")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.2.1")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -86,23 +86,23 @@ dependencies {
   // debugImplementation because LeakCanary should only run in debug builds.
   debugImplementation("com.squareup.leakcanary:leakcanary-android:2.12")
   // navigation
-  implementation("androidx.navigation:navigation-compose:2.8.9")
+  implementation("androidx.navigation:navigation-compose:2.9.1")
   // hilt
-  implementation("com.google.dagger:hilt-android:2.56")
-  ksp("com.google.dagger:hilt-compiler:2.56")
+  implementation("com.google.dagger:hilt-android:2.56.2")
+  ksp("com.google.dagger:hilt-compiler:2.56.2")
   // hilt for navigation compose
   implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
   // hilt for work manager
   implementation("androidx.hilt:hilt-work:1.2.0")
   ksp("androidx.hilt:hilt-compiler:1.2.0")
   implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
-  implementation("androidx.work:work-runtime-ktx:2.10.0")
+  implementation("androidx.work:work-runtime-ktx:2.10.2")
   // app compat (for locales)
-  val appcompatVersion = "1.7.0"
+  val appcompatVersion = "1.7.1"
   implementation("androidx.appcompat:appcompat:$appcompatVersion")
   implementation("androidx.appcompat:appcompat-resources:$appcompatVersion")
   // room db
-  val roomVersion = "2.6.1"
+  val roomVersion = "2.7.2"
   implementation("androidx.room:room-runtime:$roomVersion")
   annotationProcessor("androidx.room:room-compiler:$roomVersion")
   ksp("androidx.room:room-compiler:$roomVersion")
@@ -113,7 +113,7 @@ dependencies {
   implementation("androidx.paging:paging-compose:$pagingVersion")
 
   // datastore
-  implementation("androidx.datastore:datastore:1.1.3")
+  implementation("androidx.datastore:datastore:1.1.7")
   implementation("com.google.protobuf:protobuf-javalite:$protobufVersion")
   implementation("com.google.protobuf:protobuf-kotlin-lite:$protobufVersion")
 
@@ -124,7 +124,7 @@ dependencies {
   implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
 
   // cache
-  implementation("io.github.reactivecircus.cache4k:cache4k:0.13.0")
+  implementation("io.github.reactivecircus.cache4k:cache4k:0.14.0")
 }
 
 hilt { enableAggregatingTask = true }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
   id("com.android.library") version "8.11.0" apply false
   id("org.jetbrains.kotlin.android") version "2.1.20" apply false
   id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
-  id("com.google.dagger.hilt.android") version "2.56" apply false
+  id("com.google.dagger.hilt.android") version "2.56.2" apply false
   id("com.google.devtools.ksp") version "2.1.20-1.0.31" apply false
 }
 
